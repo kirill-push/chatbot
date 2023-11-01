2e2d4b1cbebb92b2c617f54f6df5752e104dc9be
Kirill Pushkarev <kirillpush@yandex.ru>
2023-11-01 13:42:16 +0200

    fix: mypy annotation error

--- src/chatbot/model.py
+++ src/chatbot/model.py
@@ -1,7 +1,4 @@
-from typing import Optional
-
 import torch
-from torch import Tensor
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
@@ -9,29 +6,33 @@ class DialoGPTChatbot:
     def __init__(self) -> None:
         self.tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-small")
         self.model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-small")
-        self.chat_history_ids: Optional[Tensor] = None
+        self.chat_history_ids = torch.tensor([])
+        self.first_input = True
 
     def get_response(self, user_input: str) -> str:
         new_user_input_ids = self.tokenizer.encode(
             user_input + self.tokenizer.eos_token, return_tensors="pt"
         )
 
-        bot_input_ids = (
-            torch.cat([self.chat_history_ids, new_user_input_ids], dim=-1)
-            if self.chat_history_ids is not None
-            else new_user_input_ids
-        )
+        if not self.first_input:
+            bot_input_ids = torch.cat(
+                [self.chat_history_ids, new_user_input_ids], dim=-1
+            )
+        else:
+            bot_input_ids = new_user_input_ids
 
         self.chat_history_ids = self.model.generate(
             bot_input_ids, max_length=1000, pad_token_id=self.tokenizer.eos_token_id
         )
         response = ""
         if (
-            self.chat_history_ids is not None
+            not self.first_input
             and bot_input_ids.shape[-1] < self.chat_history_ids.shape[1]
         ):
             response = self.tokenizer.decode(
                 self.chat_history_ids[:, bot_input_ids.shape[-1] :][0],
                 skipt_special_tokens=True,
             )
+        else:
+            self.first_input = False
         return response
